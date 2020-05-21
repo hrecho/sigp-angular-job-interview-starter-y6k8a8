4e2abff541d0e7c86763618d750ae08250b74a08
hrecho <hrecho@gmail.com>
2020-05-21 21:40:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sigp-angular-job-interview-starter-y6k8a8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sigp-angular-job-interview-starter-y6k8a8)
---
+++ src/app/models/models.ts
@@ -0,0 +1,17 @@
+export interface GithubApi {
+  items: GithubIssue[];
+  total_count: number;
+}
+
+export interface GithubUser {
+  login: string;
+  html_url: string;
+}
+
+export interface GithubIssue {
+  user: GithubUser;
+  created_at: string;
+  number: string;
+  state: string;
+  title: string;
+}
